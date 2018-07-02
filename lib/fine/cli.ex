9af6ca4461f84e5def2023e6e37d48d6f5d67be1
lwhile <lwhile521@gmail.com>
2018-07-02 11:05:09 +0800

    fetch fixer data

--- lib/fine/cli.ex
+++ lib/fine/cli.ex
@@ -2,9 +2,13 @@ defmodule Fine.Cli do
 
   def parse_argv(argv) do
     parse = OptionParser.parse(argv, [switches: [help: :boolean], aliases: [h: :help]])
+    IO.inspect parse
     case parse do
       { [help: true], _ , _ }
       -> :help
+
+      { [base: base, target: target], _, _}
+      -> process(base, target)
     end
   end
 
@@ -12,6 +16,10 @@ defmodule Fine.Cli do
     IO.puts """
     usage: fine <base currency> <target currency>
     """
+  end
+
+  def process(base, target) do
+    Fine.FetchData.Fixer.fetch(base, target)
   end
 
   def main(argv \\ []) do
--- lib/fine/fetch_data/fixer.ex
+++ lib/fine/fetch_data/fixer.ex
@@ -1,11 +1,24 @@
 defmodule Fine.FetchData.Fixer do
   @fix_api_base "http://data.fixer.io/api/latest"
 
-  def fill_url(base, target) do
-    
+  def setup_access_key(url) do
+    token = Application.get_env(:fine, :token_fixer)
+    "#{url}?access_key=#{token}"
   end
 
-  def fetch do
+  def setup_symbol(url, base, target) do
+    "#{url}&base=#{base}&symbols=#{target}"
+  end
 
+  def fill_url(base, target) do
+    @fix_api_base
+    |> setup_access_key
+    |> setup_symbol(base, target)
+  end
+
+  def fetch(base, target) do
+    fill_url(base, target)
+    |> HTTPoison.get
+    |> IO.inspect
   end
 end
